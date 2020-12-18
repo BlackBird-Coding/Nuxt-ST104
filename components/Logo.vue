f2849a412e3e829d64836fb54e1ab49ef6b4fd5e
BlackBird <thanadol2005@gmail.com>
2020-12-18 12:33:13 +0700

    build: edit logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <img
-      src="https://upload.wikimedia.org/wikipedia/th/f/ff/%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%AA%E0%B8%B1%E0%B8%8D%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B9%8C_%E0%B8%A8%E0%B8%A3%E0%B8%B1%E0%B8%97%E0%B8%98%E0%B8%B2.png"
-      style="height: 300px"
-    />
+    <img src="/icon.png" style="height: 300px" />
   </div>
 </template>
